23ee56ed715d7d766d27d78b95bd3f1b9fc6b580
jgnatch <nachojgutierrez@gmail.com>
2015-03-19 00:15:10 -0300

    pass properties to ui_component

--- lib/mountain_view/component_helper.rb
+++ lib/mountain_view/component_helper.rb
@@ -2,7 +2,7 @@ module MountainView
   module ComponentHelper
 
     def ui_component(slug, properties={})
-      render "components/#{slug}/#{slug}", properties
+      render "components/#{slug}/#{slug}", properties: properties
     end
 
   end
