808841cbb03f43a733513062b6159218d45a3aeb
jgnatch <nachojgutierrez@gmail.com>
2015-03-19 22:52:19 -0300

    add assets to component

--- lib/mountain_view/railtie.rb
+++ lib/mountain_view/railtie.rb
@@ -5,6 +5,11 @@ module MountainView
   class Railtie < Rails::Railtie
     railtie_name :mountain_view
 
+    initializer "mountain_view.assets" do
+      Rails.application.config.assets.paths << "#{Rails.root}/app/components"
+    end
+
+
     initializer "mountain_view.append_view_paths" do |app|
       ActiveSupport.on_load :action_controller do
         append_view_path app.root.join("app", "components")
