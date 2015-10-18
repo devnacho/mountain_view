c7e33d97a9723f4c21153d579f497cbd8913184f
GustavoCaso <gustavocaso@gmail.com>
2015-10-18 23:53:30 +0200

    make test pass

--- test/mountain_view/component_test.rb
+++ test/mountain_view/component_test.rb
@@ -16,11 +16,11 @@ class MountainViewComponentTest < ActiveSupport::TestCase
   def test_styleguide_stubs
     component = MountainView::Component.new("header")
 
-    expected_stub = {
-      id: 1,
-      title: "20 Mountains you didn't know they even existed",
-      subtitle: "Buzzfeed title"
-    }
+    expected_stub = {:meta=>"There is this different classes",
+                     :stubs=>[
+                       {:id=>1, :title=>"20 Mountains you didn't know they even existed",
+                        :subtitle=>"Buzzfeed title"},
+                       {:id=>2, :title=>"You won't believe what happened to this man at Aspen"}]}
 
     assert_instance_of Array, component.styleguide_stubs
     assert_equal expected_stub, component.styleguide_stubs.first
