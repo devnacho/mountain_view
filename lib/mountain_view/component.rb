71492e91b17247263a7aaf0e27dbfe44f1fd9b4c
GustavoCaso <gustavocaso@gmail.com>
2015-10-18 23:52:20 +0200

    Added few methods for displaying meta information and new yml format

--- lib/mountain_view/component.rb
+++ lib/mountain_view/component.rb
@@ -2,6 +2,10 @@ module MountainView
   class Component
     attr_reader :name
 
+    def self.stub_example
+      File.read(File.expand_path('../example.yml',__FILE__))
+    end
+
     def initialize(name)
       @name = name
     end
@@ -14,6 +18,14 @@ module MountainView
       YAML.load_file stubs_file
     end
 
+    def component_stubs
+      styleguide_stubs.first[:stubs]
+    end
+
+    def component_stubs?
+      component_stubs.try(:any?) || false
+    end
+
     def stubs_file
       MountainView.configuration.components_path.join(name, "#{name}.yml")
     end
@@ -22,6 +34,18 @@ module MountainView
       styleguide_stubs.is_a?(Array) && styleguide_stubs.any?
     rescue Errno::ENOENT
       false
+    end
+
+    def stubs_extra_info?
+      stubs_extra_info.try(:empty?) || false
+    end
+
+    def stubs_extra_info
+      styleguide_stubs.first[:meta]
+    end
+
+    def stubs_correct_format?
+      styleguide_stubs.first.key?(:stubs)
     end
   end
 end
