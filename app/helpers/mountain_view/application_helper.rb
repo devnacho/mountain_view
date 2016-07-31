0e4f8d51f4ca099ac5797ad4d7caa24e2a804aae
Georg G <nilsding@nilsding.org>
2016-07-31 15:55:44 +0200

    fix respond_to? signature

--- app/helpers/mountain_view/application_helper.rb
+++ app/helpers/mountain_view/application_helper.rb
@@ -12,9 +12,9 @@ module MountainView
       end
     end
 
-    def respond_to?(method)
+    def respond_to?(method, include_all = false)
       if method.to_s.end_with?("_path") || method.to_s.end_with?("_url")
-        if main_app.respond_to?(method)
+        if main_app.respond_to?(method, include_all)
           true
         else
           super
