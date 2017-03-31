35c7168b750b09b73487c1d0249901a1855014f8
Mike Rogers <me@mikerogers.io>
2017-03-31 13:08:31 -0600

    Adding integration test for styleguide customisation

--- test/integration/mounted_engine_test.rb
+++ test/integration/mounted_engine_test.rb
@@ -3,4 +3,24 @@ class MountedEngineTest < ActionDispatch::IntegrationTest
     get "/mountain_view"
     assert_response :success
   end
+
+  test "Has the styleguide route" do
+    get "/mountain_view/styleguide"
+    assert_response :success
+  end
+
+  test "Custom path can be applied to styleguide resource" do
+    styleguide_path = MountainView.configuration.styleguide_path
+    
+    # Set the new path
+    MountainView.configuration.styleguide_path = "style-guide"
+    Rails.application.reload_routes!
+
+    get "/mountain_view/style-guide"
+    assert_response :success
+
+    # Reset back to original
+    MountainView.configuration.styleguide_path = styleguide_path
+    Rails.application.reload_routes!
+  end
 end
