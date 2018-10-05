b5d8d74c10dc2f18f9c86420601b1f87d1da2000
Christine Panus <cpanus@ajla.net>
2018-10-05 18:49:33 -0500

    Add ability to set classes for the div around the example

--- lib/mountain_view/component.rb
+++ lib/mountain_view/component.rb
@@ -70,5 +70,11 @@ module MountainView
       end
     end
 
+    def example_classes component_properties
+      if component_properties.key?(:mv_classes)
+        component_properties[:mv_classes]
+      end
+    end
+
   end
 end
