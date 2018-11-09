d6b81a3b949e7ae5c2e2a990202f748a68998595
Christine Panus <christine.panus@panusventures.com>
2018-11-09 17:59:54 -0600

    Add Stub methods for description, class and title

--- lib/mountain_view/stub.rb
+++ lib/mountain_view/stub.rb
@@ -7,11 +7,23 @@ module MountainView
       @data = component_properties.except(:mv_stub_meta) || {}
     end
 
-    def meta_title
-      if @meta.title.present?
+    def meta_title(example_title, example_number)
+      if @meta.key?(:title)
         @meta.title
       else
-        () + " " + (index + 1).to_s
+        example_title + " " + (example_number + 1).to_s
+      end
+    end
+
+    def meta_description
+      if @meta.key?(:description)
+        @meta.description
+      end
+    end
+
+    def meta_classes
+      if @meta.key?(:classes)
+        @meta.classes
       end
     end
 
