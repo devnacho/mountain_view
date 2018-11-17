31f2f26aa35e774e21134a8b1904ce38818dacea
Christine Panus <christine.panus@panusventures.com>
2018-11-16 23:08:07 -0600

    rubocop style changes and tests

--- test/mountain_view/component_test.rb
+++ test/mountain_view/component_test.rb
@@ -1,20 +1,20 @@
-require "test_helper"
+require 'test_helper'
 
 class MountainViewComponentTest < ActiveSupport::TestCase
   def test_name
-    component = MountainView::Component.new("header")
+    component = MountainView::Component.new('header')
 
-    assert_equal "header", component.name
+    assert_equal 'header', component.name
   end
 
   def test_humanized_title
-    component = MountainView::Component.new("social_media_icons")
+    component = MountainView::Component.new('social_media_icons')
 
-    assert_equal "Social media icons", component.title
+    assert_equal 'Social media icons', component.title
   end
 
   def test_styleguide_stubs
-    component = MountainView::Component.new("header")
+    component = MountainView::Component.new('header')
     expected_stub = header_stub_meta
 
     assert_instance_of Hash, component.styleguide_stubs
@@ -22,33 +22,33 @@ class MountainViewComponentTest < ActiveSupport::TestCase
   end
 
   def test_extracted_stubs
-    component = MountainView::Component.new("header")
+    component = MountainView::Component.new('header')
     expected_stub = header_stub_only
     assert_instance_of Array, component.extracted_stubs
     assert_equal expected_stub, component.extracted_stubs
   end
 
   def test_component_stubs?
-    component_with_stubs = MountainView::Component.new("header")
-    component_with_empty_stub_file = MountainView::Component.new("breadcrumbs")
+    component_with_stubs = MountainView::Component.new('header')
+    component_with_empty_stub_file = MountainView::Component.new('breadcrumbs')
     component_without_stub_file =
-      MountainView::Component.new("social_media_icons")
-    compoenet_with_stubs_but_incorrect_format =
-      MountainView::Component.new("card")
+      MountainView::Component.new('social_media_icons')
+    component_with_stubs_but_incorrect_format =
+      MountainView::Component.new('card')
     assert_equal true, component_with_stubs.component_stubs?
     assert_equal false, component_without_stub_file.component_stubs?
     assert_equal false, component_with_empty_stub_file.component_stubs?
-    assert_equal true, compoenet_with_stubs_but_incorrect_format.
-      component_stubs?
+    assert_equal true, component_with_stubs_but_incorrect_format
+                           .component_stubs?
   end
 
   def test_stubs_extra_info
-    component_with_extra_info = MountainView::Component.new("header")
+    component_with_extra_info = MountainView::Component.new('header')
     component_with_empty_stub_file =
-      MountainView::Component.new("breadcrumbs")
+      MountainView::Component.new('breadcrumbs')
     component_without_stub_file =
-      MountainView::Component.new("paragraph")
-    expected_extra_info_stub = "There is this different classes"
+      MountainView::Component.new('paragraph')
+    expected_extra_info_stub = 'There is this different classes'
 
     assert_equal expected_extra_info_stub, component_with_extra_info.
       stubs_extra_info
@@ -57,13 +57,13 @@ class MountainViewComponentTest < ActiveSupport::TestCase
   end
 
   def test_stubs_extra_info?
-    component_with_stubs = MountainView::Component.new("header")
+    component_with_stubs = MountainView::Component.new('header')
     component_with_empty_stub_file =
-      MountainView::Component.new("breadcrumbs")
+      MountainView::Component.new('breadcrumbs')
     component_without_stub_file =
-      MountainView::Component.new("social_media_icons")
+      MountainView::Component.new('social_media_icons')
     component_with_stubs_but_no_extra_info =
-      MountainView::Component.new("card")
+      MountainView::Component.new('card')
 
     assert_equal true, component_with_stubs.stubs_extra_info?
     assert_equal false, component_without_stub_file.stubs_extra_info?
@@ -73,12 +73,12 @@ class MountainViewComponentTest < ActiveSupport::TestCase
   end
 
   def test_stubs_correct_format?
-    component_with_correct_stubs = MountainView::Component.new("header")
-    component_with_empty_stub_file = MountainView::Component.new("breadcrumbs")
+    component_with_correct_stubs = MountainView::Component.new('header')
+    component_with_empty_stub_file = MountainView::Component.new('breadcrumbs')
     component_without_stub_file =
-      MountainView::Component.new("social_media_icons")
+      MountainView::Component.new('social_media_icons')
     component_with_stubs_but_old_syntax =
-      MountainView::Component.new("card")
+      MountainView::Component.new('card')
 
     assert_equal true, component_with_correct_stubs.stubs_correct_format?
     assert_equal false, component_without_stub_file.stubs_correct_format?
@@ -88,101 +88,20 @@ class MountainViewComponentTest < ActiveSupport::TestCase
   end
 
   def test_stubs_file
-    component = MountainView::Component.new("header")
+    component = MountainView::Component.new('header')
 
-    expected_stubs_file = Rails.root.join("app/components/header/header.yml")
+    expected_stubs_file = Rails.root.join('app/components/header/header.yml')
     assert_equal expected_stubs_file, component.stubs_file
   end
 
   def test_stubs?
-    component_with_stubs = MountainView::Component.new("header")
-    component_without_stub_file = MountainView::Component.new("social_media_icons")
-    component_with_empty_stub_file = MountainView::Component.new("breadcrumbs")
+    component_with_stubs = MountainView::Component.new('header')
+    component_without_stub_file = MountainView::Component.new('social_media_icons')
+    component_with_empty_stub_file = MountainView::Component.new('breadcrumbs')
 
     assert_equal true, component_with_stubs.stubs?
     assert_equal false, component_without_stub_file.stubs?
     assert_equal false, component_with_empty_stub_file.stubs?
   end
-
-  #TODO: Move these tests
-  # def test_example_title
-  #   component = MountainView::Component.new("meta_header")
-  #   component.component_stubs.each_with_index do |component_properties, index|
-  #     if index.eql?(0)
-  #       assert_equal "Specific Example",
-  #                    component.example_title(component_properties, index)
-  #     else
-  #       built_title = component.title + " " + (index + 1).to_s
-  #       assert_equal built_title,
-  #                    component.example_title(component_properties, index)
-  #     end
-  #   end
-  # end
-  #
-  # def test_example_description
-  #   component = MountainView::Component.new("meta_header")
-  #   component.component_stubs.each_with_index do |component_properties, index|
-  #     if index.eql?(0)
-  #       assert_equal "This describes the use of this specific settings for "\
-  #                        "the component",
-  #                    component.example_description(component_properties)
-  #     else
-  #       assert_nil component.example_description(component_properties)
-  #     end
-  #   end
-  # end
-  #
-  # def test_example_classes
-  #   component = MountainView::Component.new("meta_header")
-  #   component.component_stubs.each_with_index do |component_properties, index|
-  #     if index.eql?(0)
-  #       assert_equal "mv--black-background",
-  #                    component.example_classes(component_properties)
-  #     else
-  #       assert_nil component.example_classes(component_properties)
-  #     end
-  #   end
-  # end
-  #
-  # def test_remove_example_meta_properties
-  #   component = MountainView::Component.new("meta_header")
-  #   component.component_stubs.each do |component_properties|
-  #     test_properties =
-  #       component.remove_example_meta_properties component_properties
-  #     refute test_properties[:mv_title]
-  #     refute test_properties[:mv_description]
-  #     refute test_properties[:mv_classes]
-  #   end
-  # end
-
-  private
-
-  def header_stub_meta
-    {
-        meta: "There is this different classes",
-        stubs:
-            [
-                {
-                    id:  1,
-                    title: "20 Mountains you didn't know they even existed",
-                    subtitle: "Buzzfeed title"
-                },
-                {
-                    id: 2,
-                    title: "You won't believe what happened to this man at Aspen"
-                }
-            ]
-    }
-  end
-
-  def header_stub_only
-    header_stub_meta[:stubs]
-  end
-
-  def header_stub_first
-    header_stub_only.first()
-  end
-
-
 
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,23 +1,58 @@
 # Configure Rails Environment
-ENV["RAILS_ENV"] = "test"
+ENV['RAILS_ENV'] = 'test'
 
 require File.expand_path("dummy/config/environment.rb", __dir__)
 require "rails/test_help"
 
-require "minitest/reporters"
+require 'minitest/reporters'
 Minitest::Reporters.use!
 
 Rails.backtrace_cleaner.remove_silencers!
 
 # Load support files
-Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
+Dir['#{File.dirname(__FILE__)}/support/**/*.rb'].each { |f| require f }
 
 # Load fixtures from the engine
 if ActiveSupport::TestCase.method_defined?(:fixture_path=)
+<<<<<<< HEAD
   ActiveSupport::TestCase.fixture_path = File.expand_path("fixtures", __dir__)
+=======
+  ActiveSupport::TestCase.fixture_path = File.expand_path('../fixtures', __FILE__)
+>>>>>>> rubocop style changes and tests
 end
 # for generators
-require "rails/generators/test_case"
-require "generators/mountain_view/component_generator"
-require "generators/mountain_view/extra_pages_generator"
+require 'rails/generators/test_case'
+require 'generators/mountain_view/component_generator'
+require 'generators/mountain_view/extra_pages_generator'
 
+def header_stub_meta
+  {
+    meta: 'There is this different classes',
+    stubs:
+      [
+        {
+          mv_stub_meta:
+            {
+                title: 'Specific Example',
+                description: 'Instructions for use case or other UX considerations',
+                classes: 'black-background'
+            },
+          id:  1,
+          title: "20 Mountains you didn't know they even existed",
+          subtitle: 'Buzzfeed title'
+        },
+        {
+          id: 2,
+          title: "You won't believe what happened to this man at Aspen"
+        }
+      ]
+  }
+end
+
+def header_stub_only
+  header_stub_meta[:stubs]
+end
+
+def header_stub_first
+  header_stub_only.first
+end
