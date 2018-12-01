ffe0dddbc0f8e65718ac0963b98e58ebe0b2cf1c
Christine Panus <christine.panus@panusventures.com>
2018-11-30 22:59:11 -0600

    Removed a bit too much
    * returned some double quotes
    * added rails/test_help back in

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,8 @@
 # Configure Rails Environment
 ENV['RAILS_ENV'] = 'test'
 
-require File.expand_path('dummy/config/environment.rb',  __dir__)
+require File.expand_path("dummy/config/environment.rb",  __dir__)
+require "rails/test_help"
 
 Rails.backtrace_cleaner.remove_silencers!
 
@@ -10,8 +11,7 @@ Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
 
 # Load fixtures from the engine
 if ActiveSupport::TestCase.method_defined?(:fixture_path=)
-  ActiveSupport::TestCase.fixture_path =
-    File.expand_path('fixtures', __dir__)
+  ActiveSupport::TestCase.fixture_path = File.expand_path("fixtures", __dir__)
 end
 # for generators
 require 'rails/generators/test_case'
