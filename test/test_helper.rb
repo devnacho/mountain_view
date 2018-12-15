3ba71922578f13a460ff4f09588a0582fdb04b9c
Esteban Pastorino <ejpastorino@gmail.com>
2018-12-15 16:27:03 -0300

    Indentation and quotes

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,5 @@
 # Configure Rails Environment
-ENV['RAILS_ENV'] = 'test'
+ENV["RAILS_ENV"] = "test"
 
 require File.expand_path("dummy/config/environment.rb", __dir__)
 require "rails/test_help"
@@ -11,33 +11,31 @@ Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
 
 # Load fixtures from the engine
 if ActiveSupport::TestCase.method_defined?(:fixture_path=)
-  ActiveSupport::TestCase.fixture_path = File.expand_path('fixtures', __dir__)
+  ActiveSupport::TestCase.fixture_path = File.expand_path("fixtures", __dir__)
 end
 # for generators
-require 'rails/generators/test_case'
-require 'generators/mountain_view/component_generator'
-require 'generators/mountain_view/extra_pages_generator'
+require "rails/generators/test_case"
+require "generators/mountain_view/component_generator"
+require "generators/mountain_view/extra_pages_generator"
 def header_stub_meta
   {
-    meta: 'There is this different classes',
-    stubs:
-      [
-        {
-          mv_stub_meta:
-            {
-              title: 'Specific Example',
-              description: 'Instructions for use case and UX considerations',
-              classes: 'black-background'
-            },
-          id:  1,
-          title: "20 Mountains you didn't know they even existed",
-          subtitle: 'Buzzfeed title'
+    meta: "There is this different classes",
+    stubs: [
+      {
+        mv_stub_meta: {
+          title: "Specific Example",
+          description: "Instructions for use case and UX considerations",
+          classes: "black-background"
         },
-        {
-          id: 2,
-          title: "You won't believe what happened to this man at Aspen"
-        }
-      ]
+        id: 1,
+        title: "20 Mountains you didn't know they even existed",
+        subtitle: "Buzzfeed title"
+      },
+      {
+        id: 2,
+        title: "You won't believe what happened to this man at Aspen"
+      }
+    ]
   }
 end
 
